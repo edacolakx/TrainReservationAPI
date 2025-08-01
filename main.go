660436b9650346a76d983b884c6770d1c7fac659
Eda Çolak <edaacolakk1907@gmail.com>
2025-08-01 19:09:48 +0300

    Update main.go

--- main.go
+++ main.go
@@ -57,7 +57,7 @@ type Data struct {
 func main() {
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Vite React uygulaması portu
+		AllowOrigins:     []string{"*"}, // Vite React uygulaması portu
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
